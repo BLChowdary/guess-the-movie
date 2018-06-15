27c16dcd6d0963cf6a8f2a53ed8b89273f6c2308
JAHNVI <jbdalwadi01@gmail.com>
2018-06-15 14:41:03 +0530

    audio related issues solved

--- s1.js
+++ s1.js
@@ -1,49 +1,56 @@
-var second=30;
-var i;
-var flag;
+var second = 30;
+var i,interval;
+var flag, ans = 0;
 var f = [0, 0, 0, 0, 0];
 var x = document.getElementsByClassName("key-option");
 var p = x[0].getElementsByClassName("key");
-
+var audio;
 window.addEventListener('keydown', function (e) {
-	var audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-if(e.keyCode>48 && e.keyCode<54){	
-	i = (e.keyCode - 49);
-	if (f[i] == 0) {
-		
-		audio.play();
-		document.getElementById("timer").innerHTML=30;
-		document.getElementById("note").innerHTML ="  ";
-        countTime();
-		f[i] = 1;
-		
-	}
+	audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	if (e.keyCode > 48 && e.keyCode < 54) {
+		i = (e.keyCode - 49);
+		if (f[i] == 0) {
 
-}
+			audio.play();
+			document.getElementById("timer").innerHTML = 30;
+			document.getElementById("note").innerHTML = "  ";
+			countTime();
+			f[i] = 1;
+			
+		}
+
+	}
 
 }
 );
 
+
+
 document.getElementById("movie").addEventListener('keydown', function (enter) {
-	
+
 
 	if (enter.keyCode == 13) {
 		var name = document.getElementById("movie");
 
 		var movies = ["raees", "humpty sharma ki dulhaniya", "main tera hero", "ek villain", "ae dil hai mushkil"];
 
+
+		//when answer is matched and time is not over
 		if (name.value == movies[i]) {
-            p[i].style.background = "green";
-            var note = document.getElementById("note").innerHTML = "yeayy!! correct";
-
-
+			p[i].style.background = "green";
+			var note = document.getElementById("note").innerHTML = "yeayy!! correct";
+			clearInterval(interval);
+		
 		}
-	
-		else{
-			p[i].style.background = "red";
-            var note = document.getElementById("note").innerHTML = "OOps!! wrong";
 
-        }
+		else if (name.value != movies[i]) //when answer does not match and time is not over
+		{
+			p[i].style.background = "red";
+			var note = document.getElementById("note").innerHTML = "OOps!! wrong";
+			document.getElementById("timer").innerHTML =" ";
+		
+			clearInterval(interval);
+		}
 	}
 
 
@@ -52,25 +59,28 @@ document.getElementById("movie").addEventListener('keydown', function (enter) {
 );
 
 
+
+
 function countTime() {
 	var zero = document.getElementById("timer").innerHTML;
 
-       setInterval( start, 1000);
+interval=setInterval(start, 1000);
 
 }
 
 
 function start() {
-    second = document.getElementById("timer").innerHTML;
-  
-if(second>=1){
-	second--;
-	flag=0;
-    document.getElementById("timer").innerHTML=second;
-}
-if(second==0){
-	flag=1;
-	 p[i].style.background = "red";
-	var note = document.getElementById("note").innerHTML = "OOps!! Time Up";
-}
+	second = document.getElementById("timer").innerHTML;
+
+	if (second >= 1) {
+		second--;
+		flag = 0;
+		document.getElementById("timer").innerHTML = second;
+	}
+	//when time is over whether the answer is correct or not ,it is not valid
+	if (second == 0) {
+		flag = 1;
+		p[i].style.background = "red";
+		var note = document.getElementById("note").innerHTML = "OOps!! Time Up";
+	}
 }
